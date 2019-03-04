90b9e839c4a2d5f234386d1d93583b17c2290bc5
Saun25 <ssaunak256@gmail.com>
2019-03-04 09:45:32 +0530

    Task2up1

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -8,23 +8,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
-        Movie movie=(Movie) context.getBean("movie");
-        System.out.println("Using Application context");
-        System.out.println(movie.toString());
-
-        ClassPathResource resource = new ClassPathResource("bean.xml");
-
-        BeanFactory factory=new XmlBeanFactory(resource);
-        Movie movie1=(Movie) factory.getBean("movie");
-        System.out.println("Using XmlBeanFactory");
-        System.out.println(movie1.toString());
-
-        BeanDefinitionRegistry definitionRegistry= new XmlBeanFactory(resource);
-        Movie movie2= (Movie)((XmlBeanFactory) definitionRegistry).getBean("movie");
-        System.out.println("Using BeanDefinitionRegistry");
-        System.out.println(movie2.toString());
+        Movie movie=(Movie) context.getBean("movieA");
+       // System.out.println(movie.toString());
+        Movie movie1=(Movie)context.getBean("movieB");
+        System.out.println(movie==movie1);
+//
+//        ClassPathResource resource = new ClassPathResource("bean.xml");
+//
+//        BeanFactory factory=new XmlBeanFactory(resource);
+//        Movie movie1=(Movie) factory.getBean("movie");
+//        System.out.println("Using XmlBeanFactory");
+//        System.out.println(movie1.toString());
+//
+//        BeanDefinitionRegistry definitionRegistry= new XmlBeanFactory(resource);
+//        Movie movie2= (Movie)((XmlBeanFactory) definitionRegistry).getBean("movie");
+//        System.out.println("Using BeanDefinitionRegistry");
+//        System.out.println(movie2.toString());
     }
 }
